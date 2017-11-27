8451f7648649e0f5263d856bd41b1305088e0dbb
Wafa AlKebsi <wafa.alkebsi@udc.edu>
2017-11-27 12:38:43 -0500

    Update pay.java

--- pay.java
+++ pay.java
@@ -4,9 +4,9 @@ import javax.swing.JOptionPane;
 public class javapay
 {
 	public static void main (String[]args)
-	{ double pay= 0;
+	{ double pay=0;
 	String name=""; 
-	int hours =0; 
+	int hours=0; 
 	double payrate=0;
 	name=JOptionPane.showInputDialog("Enter your Name");
 	hours=Integer.parseInt(JOptionPane.showInputDialog("Enter Number of hours"));
